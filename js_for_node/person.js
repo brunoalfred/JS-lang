f8d3dee819ba4acbf6f5dfecbfa75773110adf39
Bruno Alfred <alfredybruno@gmail.com>
2020-04-15 17:52:41 +0300

    implemented a function to export

--- js_for_node/person.js
+++ js_for_node/person.js
@@ -1,6 +1,12 @@
 class Person {
-    constructor(name, age){
+    constructor(name, age) {
         this.name;
         this.age;
     }
 }
+
+var greeting = function () {
+    console.log(`My name is ${this.name} and i\'m ${this.age} old`);
+}
+
+module.ex 
