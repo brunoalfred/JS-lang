31fe67929c178aad7cbbc4d6e3539eee4a0be204
Bruno Alfred <alfredybruno@gmail.com>
2020-04-21 22:55:26 +0300

    creating a server

--- js_for_node/server.js
+++ js_for_node/server.js
@@ -1,12 +1,15 @@
 // Using the custome http module for Nodejs
 const http = require('http');
-const port = process.env.PORT ;
+const port = process.env.PORT;
 
 
 
-http.createServer( (req, res) => {
-    req.statusCode = 200;
-    res.se
+http.createServer((req, res) => {
+    
+    res.statusCode =200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+    
 });
 
 
