e30452de661a62d01e09ad549364595908b4a54c
Bruno Alfred <alfredybruno@gmail.com>
2020-04-26 18:04:44 +0300

    creating the route

--- js_for_node/index.js
+++ js_for_node/index.js
@@ -1,2 +1,14 @@
 const express = require('express');
 const fs = require('fs');
+
+
+
+// the variable for port number
+const port = process.env.PORT || 3000;
+// create the app
+const app = express();
+
+//  setting up the route 
+
+
+
--- js_for_node/server.js
+++ js_for_node/server.js
@@ -1,21 +1,21 @@
-// Using the custome http module for Nodejs
-const http = require('http');
-const hostname = 3000;
-const port = process.env.PORT;
+// // Using the custome http module for Nodejs
+// const http = require('http');
+// const hostname = 3000;
+// const port = process.env.PORT;
 
 
 
-const server = http.createServer((req, res) => {
+// const server = http.createServer((req, res) => {
     
-    res.statusCode =200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+//     res.statusCode =200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hello World');
 
-});
+// });
 
-// Make the server run at URL => http://localhost:3000
-server.listen(port, hostname, () => {
+// // Make the server run at URL => http://localhost:3000
+// server.listen(port, hostname, () => {
 
-console.log(`Server Running at http://${hostname}:${port}`);
-});
+// console.log(`Server Running at http://${hostname}:${port}`);
+// });
 
